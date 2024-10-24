9ce1086f8f6da812c836b09a0945cac0b761709b
Diego Lopes <38234094+titansjd@users.noreply.github.com>
2024-10-24 09:37:46 -0300

    TCC Cuidapet - Fase 2
    Pedro solicitou alteração no seu sistema. Além da mensagem de boas-vindas, você deve alterar o algoritmo para que ele apresente um texto com o endereço e o número da loja.
    
    Você deve armazenar essas informações em variáveis. Apresente na tela a nova mensagem.

--- logica_programacao/cuidapet.dart
+++ logica_programacao/cuidapet.dart
@@ -1,4 +1,8 @@
 void main() {
-  print(
-      'Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento');
+ String enderecoLoja = 'Rua X ';
+  int numeroLoja = 666; 
+  
+  print('Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento \n ');
+  print('Endereco $enderecoLoja, $numeroLoja');
+  
 }
