c78d9064c2f57c4c7c79a12ef116ee5716c1ee30
Diego Lopes <titansjd@gmail.com>
2024-10-24 21:22:38 -0300

    TCC Cuidapet - Fase 3 (organizacao do projeto)

--- logica_programacao/cuidapet.dart
+++
@@ -1,8 +0,0 @@
-void main() {
- String enderecoLoja = 'Rua X ';
-  int numeroLoja = 666; 
-  
-  print('Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento \n ');
-  print('Endereco $enderecoLoja, $numeroLoja');
-  
-}
